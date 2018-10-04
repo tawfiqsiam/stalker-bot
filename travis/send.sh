e5d3d2f29e3b58f0b7ec53d525eee67c4922f5ed
D4RKAR117 <D4RKAR117@outlook.es>
2018-10-04 00:53:37 -0500

    Update for general layout

--- travis/send.sh
+++ travis/send.sh
@@ -8,22 +8,22 @@ echo -e "[Webhook]: Sending webhook to Discord...\\n";
 
 case $1 in
   "success" )
-    EMBED_COLOR=3066993
-    STATUS_MESSAGE="Passed"
-    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-blue.png"
-    ;;
+	EMBED_COLOR=3066993
+	STATUS_MESSAGE="Passed"
+	AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-blue.png"
+	;;
 
   "failure" )
-    EMBED_COLOR=15158332
-    STATUS_MESSAGE="Failed"
-    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-red.png"
-    ;;
+	EMBED_COLOR=15158332
+	STATUS_MESSAGE="Failed"
+	AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-red.png"
+	;;
 
   * )
-    EMBED_COLOR=0
-    STATUS_MESSAGE="Status Unknown"
-    AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-1.png"
-    ;;
+	EMBED_COLOR=0
+	STATUS_MESSAGE="Status Unknown"
+	AVATAR="https://travis-ci.com/images/logos/TravisCI-Mascot-1.png"
+	;;
 esac
 
 AUTHOR_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%aN")"
@@ -43,33 +43,34 @@ else
   URL=""
 fi
 
+# * Update based In base Script Update
 TIMESTAMP=$(date --utc +%FT%TZ)
 WEBHOOK_DATA='{
   "username": "",
   "avatar_url": "https://travis-ci.com/images/logos/TravisCI-Mascot-1.png",
   "embeds": [ {
-    "color": '$EMBED_COLOR',
-    "author": {
-      "name": "Job #'"$TRAVIS_JOB_NUMBER"' (Build #'"$TRAVIS_BUILD_NUMBER"') '"$STATUS_MESSAGE"' - '"$TRAVIS_REPO_SLUG"'",
-      "url": "https://travis-ci.com/'"$TRAVIS_REPO_SLUG"'/builds/'"$TRAVIS_BUILD_ID"'",
-      "icon_url": "'$AVATAR'"
-    },
-    "title": "'"$COMMIT_SUBJECT"'",
-    "url": "'"$URL"'",
-    "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
-    "fields": [
-      {
-        "name": "Commit",
-        "value": "'"[\`${TRAVIS_COMMIT:0:7}\`](https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT)"'",
-        "inline": true
-      },
-      {
-        "name": "Branch/Tag",
-        "value": "'"[\`$TRAVIS_BRANCH\`](https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_BRANCH)"'",
-        "inline": true
-      }
-    ],
-    "timestamp": "'"$TIMESTAMP"'"
+	"color": '$EMBED_COLOR',
+	"author": {
+	  "name": "Job #'"$TRAVIS_JOB_NUMBER"' (Build #'"$TRAVIS_BUILD_NUMBER"') '"$STATUS_MESSAGE"' - '"$TRAVIS_REPO_SLUG"'",
+	  "url": "'"$TRAVIS_BUILD_WEB_URL"'",  
+	  "icon_url": "'$AVATAR'"
+	},
+	"title": "'"$COMMIT_SUBJECT"'",
+	"url": "'"$URL"'",
+	"description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+	"fields": [
+	  {
+		"name": "Commit",
+		"value": "'"[\`${TRAVIS_COMMIT:0:7}\`](https://github.com/$TRAVIS_REPO_SLUG/commit/$TRAVIS_COMMIT)"'",
+		"inline": true
+	  },
+	  {
+		"name": "Branch/Tag",
+		"value": "'"[\`$TRAVIS_BRANCH\`](https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_BRANCH)"'",
+		"inline": true
+	  }
+	],
+	"timestamp": "'"$TIMESTAMP"'"
   } ]
 }'
 
